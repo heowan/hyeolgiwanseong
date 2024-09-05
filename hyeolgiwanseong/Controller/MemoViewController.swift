b028789213809649b4148d01b55010ad2bf3c9de
heowan <101536766+heowan@users.noreply.github.com>
2024-09-05 12:33:33 +0900

    feat: 기록화면 레이아웃 구현

---
+++ hyeolgiwanseong/Controller/MemoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MemoViewController.swift
+//  hyeolgiwanseong
+//
+//  Created by Heowan on 9/4/24.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func cancelPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
